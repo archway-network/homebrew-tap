91180baa058b5c2a32e2434a08ffe08c18c76d28
Augusto Elesbão <aelesbao@users.noreply.github.com>
2024-09-30 12:14:21 +0200

    feat: add recipe to install latest version from HEAD

--- archway.rb
+++ archway.rb
@@ -1,40 +1,29 @@
 class Archway < Formula
-  desc "The Archway Network smart contracts protocol runtime"
+  desc "Incentivized CosmWasm smart contracts blockchain runtime"
   homepage "https://archway.io"
+  url "https://github.com/archway-network/archway/releases/download/v7.0.1/archwayd_darwin_all.zip"
+  sha256 "15caa730b8ba3bd878c449cc5d0d2882b125569260a89decb3ace9328d6ca316"
   license "GPL-2.0-only"
 
-  version "7.0.1"
+  head do
+    url "https://github.com/archway-network/archway.git", branch: "main"
 
-  on_macos do
-    url "https://github.com/archway-network/archway/releases/download/v#{version}/archwayd_darwin_all.zip"
-    sha256 "15caa730b8ba3bd878c449cc5d0d2882b125569260a89decb3ace9328d6ca316"
-
-    def install
-      bin.install "archwayd"
-    end
+    depends_on "cmake" => :build
+    depends_on "go" => :build
   end
 
-  on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/archway-network/archway/releases/download/v#{version}/archwayd_linux_arm64.zip"
-      sha256 "5db3f24424d9fd4f7e55b199f04470e8a6b10fe5530f06fe0e6a7a9ed69d8f21"
-
-      def install
-        bin.install "archwayd"
-      end
+  def install
+    if build.head?
+      system "make", "build"
+      bin.install "build/archwayd"
+    else
+      bin.install "archwayd"
     end
 
-    if Hardware::CPU.intel?
-      url "https://github.com/archway-network/archway/releases/download/v#{version}/archwayd_linux_amd64.zip"
-      sha256 "c5ef072007380e300c756750e04257f164b03d7ed28adb98189a7d9af92d1cce"
-
-      def install
-        bin.install "archwayd"
-      end
-    end
+    generate_completions_from_executable(bin/"archwayd", "completion", shell_parameter_format: :flag, shells: [:zsh])
   end
 
   test do
-    assert_equal "#{version}", shell_output("#{bin}/archwayd version").strip
+    assert_match version.to_s.sub("HEAD", ""), shell_output("#{bin}/archwayd version").strip
   end
 end
